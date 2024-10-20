1b8dbeb54362073405f0554594f1b6ccf4de4876
PrasadAdv <prasadvjartarkar@gmail.com>
2024-10-20 20:55:38 +0530

    sets up expectations in test cases

--- spec/helpers/string_cal_helper_spec.rb
+++ spec/helpers/string_cal_helper_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'StringCalHelper', type: :request do
+  include StringCalHelper
+
   describe 'extract_digits' do
     it 'extracts positive and negative digits from given string' do
+      input_string = '\2\3,4;-5,2\n-4;6'
+      extracted_digits = extract_digits(input_string)
+      expect(extracted_digits).to eq(
+        { positive_digits: [2, 3, 4, 2, 6], negative_digits: [-5,-4] }
+      )
     end
   end
 end
--- spec/requests/string_cal_spec.rb
+++ spec/requests/string_cal_spec.rb
@@ -2,16 +2,35 @@ require 'rails_helper'
 
 RSpec.describe 'StringCals', type: :request do
   describe 'add' do
+    let(:str_instance) { StringCalController.new }
+
+    before do
+      allow_any_instance_of(StringCalController).to receive(:is_string_valid?).and_call_original
+    end
+
     context 'string validation' do
       it 'returns true upon successful validation' do
+        allow_any_instance_of(StringCalController).to receive(:params).and_return(ActionController::Parameters.new(string_cal: { input_string: '\2\3,4;,2\n;6' }))
+        expect(str_instance.is_string_valid?).to be true
       end
 
       it 'raises error upon failed validation' do
+        allow_any_instance_of(StringCalController).to receive(:params).and_return(ActionController::Parameters.new(string_cal: { input_string: '\2\3,4;,-2\n;-6' }))
+        expect(str_instance.is_string_valid?).to be false
       end
     end
 
     context 'digit addition' do
       it 'adds up the digits in given string' do
+        post '/add', params: { string_cal: { input_string: '1,2,3' } }
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)).to eq({ 'response' => 6 })
+      end
+
+      it 'returns error response if string is not valid' do
+        post '/add', params: { string_cal: { input_string: '1,2,-3' } }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq({ 'error' => 'negative numbers are not allowed [-3]' })
       end
     end
   end
